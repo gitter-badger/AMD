fda7b4f6f3c4b47ccc25369549fc0589fca362d3
robert <dobre_robert_marius@yahoo.com>
2015-07-09 13:10:43 +0200

    tests for closures

--- tests/unit/main.js
+++ tests/unit/main.js
@@ -1,5 +1,4 @@
 // WIP:
-// - closures
 // - dots and slashes in name - test relative paths -> SHOULD NOT BE ACCEPTED - or make a config object where you can choose if you want relative paths or not
 
 
@@ -1625,5 +1624,47 @@ test('should export method', function(assert){
 
     assert.deepEqual(klass.getNumber(), 10, 'should execute method on class');
     assert.equal(AMD.getLength(), 1, 'registry length should be 1');
+
+});
+
+
+
+QUnit.module('Closures', config);
+
+test('should export instance of an new CustomFunction', function(assert){
+
+    AMD.define('export/custom/instance', ['exports'], function(exports){
+
+        var number = 5;
+
+        var getNumber = function(){
+            return number;
+        };
+
+        var increment = function(){
+            number++;
+        };
+
+        exports["default"] = getNumber;
+        exports["increment"] = increment;
+        exports["getNumber"] = getNumber;
+
+    });
+
+    var getNumber = AMD.require('export/custom/instance', 'getNumber');
+    var increment = AMD.require('export/custom/instance', 'increment');
+
+    assert.equal(AMD.getLength(), 1, 'registry length should be 1');
+    assert.deepEqual(getNumber(), 5, 'should access method from closure');
+
+    increment();
+
+    assert.deepEqual(getNumber(), 6, 'should access private variables values from closure');
+
+    var newGetNumber = AMD.require('export/custom/instance');
+
+    increment();
+
+    assert.deepEqual(newGetNumber(), 7, 'all required requests should get the new updated value from closure');
 
 });
